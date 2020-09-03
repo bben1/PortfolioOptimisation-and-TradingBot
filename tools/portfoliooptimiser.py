e6b19a52068f9db81428b22926ee402e437923af
bben1 <70587066+bben1@users.noreply.github.com>
2020-09-03 17:18:49 +0100

    Update portfoliooptimiser.py

--- tools/portfoliooptimiser.py
+++ tools/portfoliooptimiser.py
@@ -1,19 +1,3 @@
-from pandas_datareader import data as web
-from pandas.util.testing import assert_frame_equal
-import pandas as pd
-import numpy as np
-import datetime as datetime
-
-import matplotlib.pyplot as plt
-plt.style.use('fivethirtyeight')
-
-from pypfopt.efficient_frontier import EfficientFrontier, objective_functions
-from pypfopt import risk_models
-from pypfopt import expected_returns
-from pypfopt.discrete_allocation import DiscreteAllocation, get_latest_prices
-
-
-
 class Portfolio:
     """
     Summary:
